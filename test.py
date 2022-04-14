96d54fe3e193a91981322b40ec54505ce4edea83
yiyoori <yiyoori05@gmail.com>
2022-04-14 13:09:43 +0900

    파이썬 연습_1

---
+++ test.py
@@ -0,0 +1,38 @@
+arr= [ 1,2,3 ] #배열
+print (arr)
+#중복 됨
+
+dic={1,2,3} #딕셔너리
+print(dic)
+#중복 안됨
+
+int #정수
+
+str="임재연" #문자열
+print(str)
+
+nun2=1.2
+
+print(nun2)
+
+
+num3=10
+num4=20
+
+if 10>20:
+    print( "여자")
+else:
+    print("남자")
+
+
+if num3>num4:
+    print("10")
+else:
+    print("20")
+
+
+#슬라이싱
+str="가나다라마바사"
+print(str[3:]) # 3부터 모두 출력
+print(str[2:3]) # 2~3 출력
+print(str[-3])  # 뒤에서 3번째 출력
